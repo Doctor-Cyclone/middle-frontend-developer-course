88b75e31cb3df25258e158681e0dabd31b2b22c3
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-03-12 15:56:44 +0300

    fix useToggle hook

--- src/custom-hooks/useToggle.ts
+++ src/custom-hooks/useToggle.ts
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
-const useToggle = (defaultValue: string[] = ['true', 'false']): [string, (newValue?: string) => void] => {
-	const [value, setValue] = useState(defaultValue[0]);
+const useToggle = (defaultValue: string[] = []): [string | boolean, (newValue?: string) => void] => {
+	const [value, setValue] = useState<string | boolean>(defaultValue[0] || false);
+	const isEmptyDefaultValue = defaultValue.length === 0;
 
 	const isLastValue = (index: number) => defaultValue.length - 1 === index
 
-	const toggleValue = (newValue?: string) => {
+	const toggleValue = (newValue?: string): void => {
 		if (newValue) {
 			setValue(newValue);
-			return;
-		}
+		} else if (isEmptyDefaultValue) {
+			setValue(!value);
+		} else if (typeof value === 'string') {
+			const currentValueIndex = defaultValue.indexOf(value);
 
-		const currentValueIndex = defaultValue.indexOf(value);
-		if (isLastValue(currentValueIndex)) {
-			setValue(defaultValue[0]);
-		} else {
-			setValue(defaultValue[currentValueIndex + 1])
+			if (isLastValue(currentValueIndex)) {
+				setValue(defaultValue[0]);
+			} else {
+				setValue(defaultValue[currentValueIndex + 1])
+			}
 		}
 	}
 
